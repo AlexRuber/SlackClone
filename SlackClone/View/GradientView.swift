c3ed969bff69981ef34994716834c96db59ed2ff
Mihai Ruber <>
2017-12-29 14:17:33 -0800

    Updated Channel VC UI and added constraints

---
+++ SlackClone/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  SlackClone
+//
+//  Created by Mihai Ruber on 12/28/17.
+//  Copyright © 2017 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
