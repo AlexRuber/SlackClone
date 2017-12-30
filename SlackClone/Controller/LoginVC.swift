ec84a0b2866227848423ef3ee367bd4a47203622
Mihai Ruber <>
2017-12-30 13:32:12 -0800

    Login UI implemented

---
+++ SlackClone/Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  SlackClone
+//
+//  Created by Mihai Ruber on 12/30/17.
+//  Copyright © 2017 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //Outlets
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ SlackClone/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SlackClone
+//
+//  Created by Mihai Ruber on 12/29/17.
+//  Copyright © 2017 Mihai Ruber. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
