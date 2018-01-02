5fce07ac109587f99aaf353a0afe9c25ff0f5349
Mihai Ruber <>
2018-01-01 21:33:31 -0800

    Added channels JSON parsing

---
+++ SlackClone/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  SlackClone
+//
+//  Created by Mihai Ruber on 1/1/18.
+//  Copyright © 2018 Mihai Ruber. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+
+    
+}
