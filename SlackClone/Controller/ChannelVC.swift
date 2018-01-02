d5bd6ca2acd5d43a0f6637e7290dbdb3cfc15c9e
Mihai Ruber <>
2018-01-01 20:53:00 -0800

    Logging Logic

--- SlackClone/Controller/ChannelVC.swift
+++ SlackClone/Controller/ChannelVC.swift
@@ -20,6 +20,10 @@ class ChannelVC: UIViewController {
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setUpUserInfo()
+    }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         if AuthService.instance.isLoggedIn {
@@ -32,11 +36,15 @@ class ChannelVC: UIViewController {
     }
     
     @objc func userDataDidChange(_ notif: Notification) {
+        setUpUserInfo()
+    }
+    
+    func setUpUserInfo() {
         if AuthService.instance.isLoggedIn {
             loginBtn.setTitle(UserDataService.instance.name, for: .normal)
             userImg.image = UIImage(named: UserDataService.instance.avatarName)
             userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
-             
+            
         } else {
             loginBtn.setTitle("Login", for: .normal)
             userImg.image = UIImage(named: "menuProfileIcon")
--- SlackClone/Utilities/Constants.swift
+++ SlackClone/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://slackchatapp.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)/user/byEmail/"
 
 // Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3921568627, blue: 0.7254901961, alpha: 0.5)
@@ -35,4 +36,7 @@ let USER_EMAIL = "userEmail"
 
 // Headers
 let HEADER = ["Content-Type": "application/json; chars=utf-8"]
+
+let BEARER_HEADER = ["Authorization": "Bearer \(AuthService.instance.authToken)", "Content-Type": "application/json; chars=utf-8"]
+
 
